4ca05c04802f8eca3c46daf7157372e21fb0b09a
Stefanos Leventis <sigianas@yahoo.com>
2023-11-23 22:24:01 +0200

    basic layout

---
+++ index.js
@@ -0,0 +1,17 @@
+const genre = document.querySelector('#genre')
+const key = document.querySelector('#key')
+const scale = document.querySelector('#scale')
+const submitBtn = document.querySelector('#submit')
+
+const genreList = ['Drill','Boom Bap','GFunk', 'Trap', 'LoFi', 'AfroBeats', 'Baile Funk', 'Club Banger']
+const keyList = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+const scaleList = ['Major','Minor', 'Melodic Minor', 'Harmonic Minor']
+
+submitBtn.addEventListener('click', ()=> {
+    const randomGenreIndex = Math.floor(Math.random()*genreList.length)
+    const randomKeyIndex = Math.floor(Math.random()*keyList.length)
+    const randomScaleIndex =Math.floor(Math.random()*scaleList.length)
+    genre.innerHTML = genreList[randomGenreIndex]
+    key.innerHTML = keyList[randomKeyIndex]
+    scale.innerHTML = scaleList[randomScaleIndex]
+})
